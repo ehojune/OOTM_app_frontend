f90450a7452f1a12f08fc59cfede8aa55c7193f2
Toby <rlaxodntttt@kaist.ac.kr>
2021-07-12 00:31:40 +0900

    make post page layout

--- frontend/app/src/main/java/com/example/cloth_recommender/Frag2/ViewPostActivity.java
+++ frontend/app/src/main/java/com/example/cloth_recommender/Frag2/ViewPostActivity.java
@@ -14,6 +14,7 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.Toast;
@@ -31,7 +32,7 @@ public class ViewPostActivity extends Activity {
 
     private ImageView img;
     private EditText cmt;
-    private Button btn;
+    private ImageButton btn;
     private static final String TAG = "ViewPostActivity";
     private String mCurrentPhotoPath;
 
@@ -55,7 +56,7 @@ public class ViewPostActivity extends Activity {
 
         popUp = new PopupWindow(this);
 
-        img = findViewById(R.id.image_big);
+        img = findViewById(R.id.postimage);
         //cmt = findViewById(R.id.comment);
         btn = findViewById(R.id.saveImage);
 
